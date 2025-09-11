ebc7f3952119105fc3fa54cab8cb77cf4f2b47ad
Vaishuu0405 <22r91a7325@tkrec.ac.in>
2025-09-11 20:07:11 +0530

    datatypes

---
+++ tnsjava/src/com/tnisf/second/dataTypes.java
@@ -0,0 +1,23 @@
+package com.tnisf.second;
+
+public class dataTypes {
+	    public static void main(String[] args) {
+	        byte byteVar = 100;
+	        short shortVar = 20000;
+	        int intVar = 100000;
+	        long longVar = 10000000000L; 
+	        float floatVar = 3.14f; 
+	        double doubleVar = 3.141592653589793;
+	        char charVar = 'A';
+	        boolean boolVar = true;
+	        System.out.println("Byte value: " + byteVar);
+	        System.out.println("Short value: " + shortVar);
+	        System.out.println("Int value: " + intVar);
+	        System.out.println("Long value: " + longVar);
+	        System.out.println("Float value: " + floatVar);
+	        System.out.println("Double value: " + doubleVar);
+	        System.out.println("Char value: " + charVar);
+	        System.out.println("Boolean value: " + boolVar);
+	    }
+	}
+
