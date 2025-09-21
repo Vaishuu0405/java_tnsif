57af0b53edc22bccfbfa24cae88b1052da9dc42c
Vaishuu0405 <22r91a7325@tkrec.ac.in>
2025-09-21 22:05:21 +0530

    final

---
+++ tnsjava/src/com/tnsif/final_member/FinalMain.java
@@ -0,0 +1,9 @@
+package com.tnsif.final_member;
+
+public class FinalMain {
+	public static void main(String[] args) {
+        Parent p = new Parent();
+        System.out.println("MAX value: " + p.MAX);
+        p.display();
+	}
+}
---
+++ tnsjava/src/com/tnsif/final_member/Parent.java
@@ -0,0 +1,8 @@
+package com.tnsif.final_member;
+
+public final class Parent {
+	 final int MAX = 100;
+	    final void display() {
+	        System.out.println("Final method in Parent, MAX = " + MAX);
+	    }
+	}
