c57939b1cc1488d36c00c86c67a82eda93eb0c7a
Vaishuu0405 <22r91a7325@tkrec.ac.in>
2025-09-21 21:49:42 +0530

    interface

---
+++ tnsjava/src/com/tnsif/interfaces/InterfaceMain.java
@@ -0,0 +1,18 @@
+package com.tnsif.interfaces;
+interface Animal {
+    void sound(); 
+}
+class Dog implements Animal {
+   
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InterfaceMain {
+	public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.sound(); 
+    }
+
+}
